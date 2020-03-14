0441005de575a96ebd7982105a0a3857b5b19d47
Thomas Raleigh <raleigh.thomas@gmail.com>
2020-03-14 00:23:25 -0700

    Force stream flush

--- Main.hs
+++ Main.hs
@@ -1,21 +1,25 @@
 import qualified Streaming.Prelude as S
 import Streaming.UDP
 import Control.Monad.Trans.Resource
+import Control.Monad.IO.Class
 import qualified Data.ByteString as B
 import System.Environment
+import System.IO
+
+printNow a = putStrLn a >> hFlush stdout
 
 main = do
   args <- getArgs
   case args of
     [host, portStr] -> case reads portStr of
-      [] -> putStrLn "Invalid port"
+      [] -> printNow "Invalid port"
       (port, _):_ -> netcat host port
-    otherwise -> putStrLn
+    otherwise -> printNow
       "Please specify host and port, e.g.: \n\t./statsd.sh 127.0.0.1 8125"
 
 netcat host port = do
-  putStrLn $ "Receiving on "<>host<>":"<>(show port)
-  runResourceT $ S.stdoutLn $ S.map txfm udpStream
+  printNow $ "Receiving on "<>host<>":"<>(show port)
+  runResourceT $ S.mapM_ (liftIO . printNow) $ S.map txfm udpStream
   where
     udpStream = fromUDP host port len
     txfm = (map (toEnum.fromEnum) . B.unpack) . fst
